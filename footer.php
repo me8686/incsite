7a372baa0fd24b5ebb7b8592516ec9f610a7f94a
me8686 <mmdebgit@gmail.com>
2024-11-16 22:40:20 +0330

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <section>
+        <span>شماره تماس</span>
+        <p>09337880913</p>
+    </section>
+    <section>
+        <span class="alic">کپی با ذکر منبع</span>
+    </section>
+    <section>
+        <span>نمادهای اعتماد</span>
+        <p>تصویر استایل نگرفت</p>
+    </section>
+</footer>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه موبایل</title>
+    <link rel="stylesheet" href="siteghaleb.css">
+</head>
+<body>
+    <?php include('header.php'); ?> <!-- گنجاندن هدر -->
+    
+    <main>
+        <aside>
+            <span>لینک‌های مفید</span>
+            <p class="font1">لینک های مفید</p>
+        </aside>
+        <section>
+            <span>محصولات</span>
+            <p>شیائومی</p>
+            <p>سامسونگ</p>
+            <p>اپل</p>
+            <p>انر</p>
+        </section>
+    </main>
+    
+    <?php include('footer.php'); ?> <!-- گنجاندن فوتر -->
+</body>
+</html>
